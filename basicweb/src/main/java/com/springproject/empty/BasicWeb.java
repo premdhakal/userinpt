5f67fac4371e1480a8a371a04ff24eedae9579e1
prmd <prmd@user2>
2020-12-03 23:45:52 -0500

    initial

---
+++ basicweb/src/main/java/com/springproject/empty/BasicWeb.java
@@ -0,0 +1,13 @@
+package com.springproject.empty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWeb.class, args);
+	}
+
+}
---
+++ basicweb/src/test/java/com/springproject/empty/EmptyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.empty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
