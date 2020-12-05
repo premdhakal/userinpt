97c1c6030654ca81de739e81dafa34ca4596aecc
prmd <prmd@user2>
2020-12-05 02:19:49 -0500

    User Entry commit

--- basicweb/src/main/java/com/springproject/empty/UserInput.java
+++ basicweb/src/main/java/com/springproject/empty/UserInput.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicWeb {
+public class UserInput {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BasicWeb.class, args);
+		SpringApplication.run(UserInput.class, args);
 	}
 
 }
